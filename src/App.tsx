00f62ea87cc81cf76a2437394939661946d515f8
BradleyJrichardson <brdl.richardson@gmail.com>
2020-01-10 19:09:52 +1100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface AppProps {}
 
- const App: React.FC<AppProps> = ({}) => {
-  return null;
+const numRows = 50;
+const numCols = 50;
+
+const App: React.FC<AppProps> = ({}) => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i <= numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+
+  console.log(grid);
+
+  return <div>helooooo</div>;
 };
 
-export default
+export default App;
